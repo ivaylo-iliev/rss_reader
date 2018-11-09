73919abbb87d09bdc431c7994c0b7baf411019b2
ivaylo-iliev <iliev.ivailo@gmail.com>
2018-11-09 17:45:31 +0200

    RSpec/Capybara tests added

---
+++ spec/features/feeds_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+# require 'feed/feed'
+
+feature "Visit feeds" do
+  scenario "able to open the list of feeds", js: true do
+    visit("/")
+    expect(page).to have_content ("Feeds")
+  end
+end
+
+feature "Add new feed" do
+  scenario "able to create new feed", js: true do
+    visit("/feeds/new")
+    fill_in 'Name', with: "CNN"
+    fill_in 'Url', with: "http://rss.cnn.com/rss/cnn_topstories.rss"
+    fill_in 'Description', with: "CNN"
+    click_button 'Save'
+    expect(page).to have_content("Feed was successfully created.")
+  end
+end
+
+feature "Reload all feeds" do
+  scenario "all feeds reloaded on demand", js: true do
+    visit("/feeds?reload_all=true")
+    expect(page).to have_content("All feeds were successfully updated.")
+  end
+end
---
+++ spec/feed/feed.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+class Feed
+
+
+  def visit_feed
+    visit('/')
+  end
+end
